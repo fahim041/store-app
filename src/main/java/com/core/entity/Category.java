a26bdde7f71349a49646ca961a23ec6fba8f9f84
fahim041 <fahimalsafi@gmail.com>
2025-05-24 11:55:18 -0600

    category entity

---
+++ src/main/java/com/core/entity/Category.java
@@ -0,0 +1,15 @@
+package com.core.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Byte id;
+
+    @Column(name = "name")
+    private String name;
+}
