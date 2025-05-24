56c4206185fcbbd3023d9b9bfaaac031dc7009f5
fahim041 <fahimalsafi@gmail.com>
2025-05-24 14:57:03 -0600

    user repository

--- src/main/java/com/core/entity/User.java
+++ src/main/java/com/core/entity/User.java
@@ -1,10 +1,7 @@
 package com.core.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -15,6 +12,7 @@ import java.util.Set;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "users")
 public class User {
